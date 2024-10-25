bb7b908ee6bc03f60c7231e3635ae537bfb92d0c
maskrpone <deparis.hipp@gmail.com>
2024-10-25 14:42:52 +0200

    Authentication fixed

--- drive/urls.py
+++ drive/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path("", views.home, name="home"),
     path('folder/<str:username>/<path:path>/', views.folder_contents, name='folder_contents'),
+    path('upload/<str:username>/', views.upload, name='upload_root'),
+    path('upload/<str:username>/<path:path>', views.upload, name='upload')
 ]
--- drive/views.py
+++ drive/views.py
@@ -1,21 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponseBadRequest
+from django.conf import settings
 import os
+
+MAX_FILE_SIZE = 40 * 1024 * 1024 # 40 Mo en octets
 
 def home(request, path="", username="maskrpone"):
 
     base_path = os.path.join("storage", username)
-
-    # Construire le chemin complet en combinant le chemin de base avec le sous-dossier
     full_path = os.path.join(base_path, path)
+    
     files_and_dirs = os.listdir(full_path)
     
     items = []
     for item in files_and_dirs:
         item_full_path = os.path.join(full_path, item)
         if os.path.isdir(item_full_path):
-            items.append({"name": item, "is_dir": True})  # C'est un dossier
+            items.append({"name": item, "is_dir": True})
         else:
-            items.append({"name": item, "is_dir": False})  # C'est un fichier
+            items.append({"name": item, "is_dir": False})
 
     context = {
         "items": items,
@@ -25,5 +28,28 @@ def home(request, path="", username="maskrpone"):
     }
     return render(request, "drive/home.html", context)
 
-def folder_contents(request, username, path):
-    return home(request,path, username)
+def folder_contents(request, username, path=""):
+    return home(request, path, username)
+
+def upload(request, username, path=""):
+    # On n'accepte que les requêtes POST, un fichier est aussi nécessaire
+    if request.method != "POST":
+        return HttpResponseBadRequest("Invalid request: POST method.")
+    if request.FILES.get("filename") is None:
+        return HttpResponseBadRequest("Invalid request: file required.")
+    
+    file = request.FILES["filename"]
+    # Le fichier ne doit pas dépasser 40Mo
+    if file.size > MAX_FILE_SIZE:
+        return HttpResponseBadRequest("File size exceed 40Mo.")
+    
+    # Définition du chemin jusqu'au fichier
+    base_path = os.path.join("storage", username)
+    full_path = os.path.join(base_path, path)
+    file_path = os.path.join(full_path, file.name)
+    
+    with open(file_path, "wb+") as destination:
+        for chunk in file.chunks():
+            destination.write(chunk)
+        
+    return redirect("folder_contents", username=username, path=path)
