c3adda744b1a190f3a6003905526ca8a3ad4b3af
anaellemzn <anaelle.mazouni@student.junia.com>
2024-10-30 15:02:32 +0100

    folder cannot exceed 100MB

--- drive/views/upload.py
+++ drive/views/upload.py
@@ -5,6 +5,8 @@ from django.core.files.storage import FileSystemStorage
 from django.http import HttpResponseBadRequest
 from ..forms import FileUploadForm
 
+MAX_STORAGE_LIMIT = 100 * 1024 * 1024  # 100 MB
+
 def upload_file(request):
     if request.method == "POST":
         form = FileUploadForm(request.POST, request.FILES)
@@ -21,6 +23,9 @@ def upload_file(request):
             uploaded_file = request.FILES["file"]
             if uploaded_file.size > 40 * 1024 * 1024:
                 return HttpResponseBadRequest("File size exceeds 40MB limit")
+            user_folder_size = get_user_folder_size(folder_path)
+            if user_folder_size + uploaded_file.size > MAX_STORAGE_LIMIT:
+                return HttpResponseBadRequest("Storage limit of 100MB exceeded")
             fs = FileSystemStorage(location=folder_path)
             fs.save(uploaded_file.name, uploaded_file)
         else:
@@ -28,3 +33,13 @@ def upload_file(request):
         if path:
             return redirect("drive", path=path)
     return redirect("drive_root")
+
+
+def get_user_folder_size(user_folder_path):
+    """Calculer la taille totale des fichiers dans le dossier de l'utilisateur."""
+    total_size = 0
+    for dirpath, dirnames, filenames in os.walk(user_folder_path):
+        for f in filenames:
+            fp = os.path.join(dirpath, f)
+            total_size += os.path.getsize(fp)
+    return total_size
