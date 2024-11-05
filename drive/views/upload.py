1da1758c651e69653156bb62241527acaa608994
Ines PINGAULT <132262728+ines-pglt@users.noreply.github.com>
2024-11-05 17:59:49 +0100

    Partie CSS des pages login et sign up finit

--- drive/views/upload.py
+++ drive/views/upload.py
@@ -10,12 +10,28 @@ from drive.views.folder import get_parent_folder
 
 MAX_STORAGE_LIMIT = 100 * 1024 * 1024  # 100 MB
 
+def get_user_folder_size(user: User) -> int:
+    """Calculer la taille totale des fichiers dans le dossier de l'utilisateur."""
+    
+    list_files = File.objects.filter(owner=user.id) # We retrieve all the files owned by a specific user
+    
+    # We sum their sizes
+    actual_used_capacity = 0
+    for file in list_files:
+        actual_used_capacity += file.size
+    
+    print(f"actual used space for {user} : {actual_used_capacity}")
+    return actual_used_capacity 
+
 def upload_file(request: HttpRequest) -> HttpResponse:
     """This is the view for file uploading"""
     if request.method == "POST":
+        print(f"test")
         form = FileUploadForm(request.POST, request.FILES) # We get the form to check if it is valid
         path = request.POST.get("current_path") # We get the current path (where we want to place the uploaded file)
         base_dir = os.path.join(settings.MEDIA_ROOT, request.user.username) # We form the root directory of the actual user
+        
+        print(f'base_dir : {base_dir}')
         
         # If our path is different from the root, we constitute it
         if path:
@@ -23,16 +39,18 @@ def upload_file(request: HttpRequest) -> HttpResponse:
         else:
            folder_path = base_dir
         
+        print(f'path: {path}')
+        
         os.makedirs(folder_path, exist_ok=True) # ensures that our target folder exists
         
         # We must make sure that our form is valid before trying to upload the file
         if form.is_valid():
             uploaded_file = request.FILES["file"] # We get the file
-            
+            print(f"uploaded file : {uploaded_file.name}")
             # We check if the file is larger than 40 Mo
             if uploaded_file.size > 40 * 1024 * 1024:
                 return HttpResponseBadRequest("File size exceeds 40MB limit")
-            
+            print(f'passed the size')
             # We check if the allocated size (100 Mo) for a user is exceeded or not 
             user_used_capacity = get_user_folder_size(request.user)
             if user_used_capacity + uploaded_file.size > MAX_STORAGE_LIMIT:
@@ -57,15 +75,4 @@ def upload_file(request: HttpRequest) -> HttpResponse:
     return redirect("drive_root")
 
 
-def get_user_folder_size(user: User) -> int:
-    """Calculer la taille totale des fichiers dans le dossier de l'utilisateur."""
-    
-    list_files = get_list_or_404(File, owner=user.id) # We retrieve all the files owned by a specific user
-    
-    # We sum their sizes
-    actual_used_capacity = 0
-    for file in list_files:
-        actual_used_capacity += file.size
-    
-    print(f"actual used space for {user} : {actual_used_capacity}")
-    return actual_used_capacity
+
