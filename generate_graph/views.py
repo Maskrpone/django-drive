0d3c5ee924bd52c5492be43f601a98a09a7589d5
maskrpone <deparis.hipp@gmail.com>
2024-11-05 16:11:34 +0100

    removed the button for chart generation

--- generate_graph/views.py
+++ generate_graph/views.py
@@ -3,7 +3,10 @@ from django.shortcuts import render
 from .generate_chart import generate_combined_chart
 
 def account_info(request):
-    return render(request, 'account_info.html')
+    folder_path = "storage/hippolyte"
+    chart_base64 = generate_combined_chart(folder_path)
+    
+    return render(request, 'account_info.html', {'chart': chart_base64})
 
 def generate_graph_ajax(request):
     folder_path = "storage/hippolyte"
