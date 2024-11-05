bde643efd581efc46063d03f119f1a5db8d904e2
anaellemzn <anaelle.mazouni@student.junia.com>
2024-11-05 18:23:52 +0100

    start home

--- drive/static/js/script.js
+++ drive/static/js/script.js
@@ -1,14 +1,25 @@
-$(document).ready(function(){
-  
-    $('[data-behavior="file-settings"]').click(function(){
-      let had = ($(this).parent().find('.file-menu').hasClass('collapsed'));
-      $('.file-menu').addClass('collapsed');
-      if (had) 
-        $(this).parent().find('.file-menu').removeClass('collapsed');
-    });
-    
-    
-    $('.file-menu li').click(function(){
-      $(this).parent().addClass('collapsed');
-    })
-  })
+// Afficher le menu pour le bouton "+"
+document.querySelector('.new-button').addEventListener('click', function() {
+  document.querySelector('.add-menu').style.display = 'block';
+});
+
+// Fermer le menu "+" en cliquant ailleurs
+document.addEventListener('click', function(e) {
+  if (!e.target.closest('.new-button') && !e.target.closest('.add-menu')) {
+    document.querySelector('.add-menu').style.display = 'none';
+  }
+});
+
+// Ouverture du modal
+document.querySelectorAll('.add-menu ul li').forEach(item => {
+  item.addEventListener('click', function() {
+    document.getElementById('modal-background').style.display = 'flex';
+  });
+});
+
+// Fermeture du modal en cliquant sur le fond
+document.getElementById('modal-background').addEventListener('click', function(e) {
+  if (e.target.id === 'modal-background') {
+    document.getElementById('modal-background').style.display = 'none';
+  }
+});
